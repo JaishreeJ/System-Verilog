4ab23726bb83ac9237726a9611f02dc105a521c6
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-04 11:41:38 +0530

    Create long int.sv

---
+++ Day 1/two and four state/long int.sv
@@ -0,0 +1,16 @@
+
+
+module data_type_longint;  
+ longint longint_data;
+initial begin
+  
+  $display("\n//longint is 64-bit signed ");
+  $display("\nBefore initialization Default value of longint = %0b",longint_data);
+  
+  longint_data = 64'b11x01xz001x01010; 
+
+  $display("\nAfter initialization value of longint = %0b\n", longint_data);
+
+ end
+endmodule
+
