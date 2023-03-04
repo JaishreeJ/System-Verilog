b2c637eaa9494d2c7f3f53cb486bfa43a7c5197c
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-04 11:46:51 +0530

    Create real.sv

---
+++ Day 1/two and four state/real.sv
@@ -0,0 +1,15 @@
+
+module data_type_real; 
+ real real_data;
+initial begin
+  
+  $display("\n//real is 64-bit real number ");
+  $display("\nBefore initialization Default value of real = %0d",real_data);
+  
+  real_data =4.43; 
+
+  $display("\nAfter initialization value of real = %f\n", real_data);
+
+ end
+endmodule
+
