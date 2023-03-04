2597f1d5391a49b3d147a250ffdcc82b1c53cab6
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-04 17:03:53 +0530

    Create protected.sv

---
+++ day 3/encapsulation/protected.sv
@@ -0,0 +1,34 @@
+class parent;
+int a,b;
+protected int c;
+protected int d=5;
+function int sum(int val1,val2);
+c=val1+val2;
+return c;
+endfunction
+function void display();
+$display("sum=%d",c);
+$display("d=%d",d);
+endfunction;
+endclass
+
+class child extends parent;
+function void disp();
+$display("2.d=%d",d);
+endfunction
+endclass
+module prot();
+parent p;
+child c1;
+int e;
+initial
+begin
+p=new();
+c1=new();
+e=p.sum(5,10);
+p.display();
+c1.disp();
+end
+endmodule
+
+
