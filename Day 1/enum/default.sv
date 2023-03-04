e806f7197d257dfde2ca531154636de5a5da028a
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-04 11:57:09 +0530

    Create default.sv

---
+++ Day 1/enum/default.sv
@@ -0,0 +1,11 @@
+module default_value_enum;
+enum {MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY }days;
+initial begin
+  days = days.first;
+ for(int i=0;i<7;i++) begin
+    $display("Days name = %0s  and its default value is = %0d",days.name,days);
+    days = days.next;
+  end
+ end
+ endmodule 
+
