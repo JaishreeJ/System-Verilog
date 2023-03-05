63abc90750881ea2035316b5868b9085cc4dce36
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-05 21:56:32 +0530

    Update return_void.sv

--- Day 2/function/return_void.sv
+++ Day 2/function/return_void.sv
@@ -1,12 +1,14 @@
 module func_return_void;
-  
+  string str="new";
   initial
   begin
     display("\t ----output for function void return type-----");
     display("\t passing string to function for displaying");
+    void '(display(str));
   end
   function void display(string str);
     $display("%s",str);
   endfunction : display
 
 endmodule : func_return_void
+
