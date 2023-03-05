abb5127512707e48c6c408636b84bae6176d469c
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-05 19:22:11 +0530

    Create if_else.sv

---
+++ Day 2/conditional/if_else.sv
@@ -0,0 +1,13 @@
+module if_else;
+byte a;
+initial begin
+  a = -1;
+  $display ("Assign the value of a = %0d ",a);
+  if(a>0)begin
+    $display ("a is a postive number");
+  end 
+  else 
+    $display ("Number is negative");
+  $display ("Out of if else block");
+end 
+endmodule 
