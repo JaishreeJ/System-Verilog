8a3113921bbe6d44f6ba51f562e488e9eddb1bb1
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-05 20:39:25 +0530

    Create foreach_for.sv

---
+++ Day 2/loops/foreach/foreach_for.sv
@@ -0,0 +1,16 @@
+
+module foreach_using_for();
+
+  int array[5];
+
+  initial 
+  begin 
+   for(int i=0;i<$size(array);i++)
+    begin 
+      array[i]=i;
+      $display("\tarray[%0d]=%0d",i,array[i]);
+    end
+    $display("\n\t----outside for loop----\n");
+  end 
+
+endmodule
