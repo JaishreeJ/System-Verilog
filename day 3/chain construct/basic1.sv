de1e2935b71f5553514f37dec124c5f4ec811b75
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-05 12:56:52 +0530

    Create basic1.sv

---
+++ day 3/chain construct/basic1.sv
@@ -0,0 +1,33 @@
+
+class parent;
+  int a;
+  function new();
+    a = 1;
+  endfunction
+  extern function void display();
+endclass:parent
+class child extends parent;
+  int b;
+  function new();
+    b = 2;
+  endfunction
+  extern function void display();
+endclass:child
+function void parent::display();
+  $display("a = %0d",a);
+endfunction
+function void child::display();
+  super.display(); 
+  $display("b = %0d",b);
+endfunction
+
+module basic_chain_construct();
+child c;
+
+initial begin
+  c = new;
+  c.display(); 
+end
+
+endmodule:basic_chain_construct
+
