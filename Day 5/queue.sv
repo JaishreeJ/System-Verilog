97f3e62e097f97407edff3ad3d997c455de86176
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-07 08:44:01 +0530

    Create queue.sv

---
+++ Day 5/queue.sv
@@ -0,0 +1,16 @@
+// Code your testbench here
+// or browse Examples
+class class_1;
+  rand bit [5:0]que[$];
+  constraint que_const{que.size()==5;}
+endclass
+module mod;
+class_1 pack =new();
+initial begin 
+ void'(pack.randomize());
+  for (int i = 0;i<=pack.que.size();i++)begin
+   $display ("Iteration =%0d The value of array  =%0p",i,pack.que[i]);
+  
+  end 
+end 
+endmodule 
