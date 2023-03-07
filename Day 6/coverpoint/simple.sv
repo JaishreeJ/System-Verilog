5ef276c5c925b503589001ad44b896ce0ac46722
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-03-07 21:20:50 +0530

    Update simple.sv

--- Day 6/coverpoint/simple.sv
+++ Day 6/coverpoint/simple.sv
@@ -2,17 +2,17 @@ module cover_point();
 bit [3:0] a;
 bit [3:0] arr[4]='{2,5,0,12};
 
-covergroup cg; //created covergroup cg 
-  a1:coverpoint a; //declared coverpoint a1
+covergroup cg;
+  a1:coverpoint a; 
 endgroup
 
-cg cg_inst; //instatiated covergroup cg
+cg cg_inst; 
 
 initial begin
   cg_inst = new();
   foreach(arr[i]) begin
-    a=arr[i]; //assigning array values to a
-    cg_inst.sample(); //sampling the covergroup
+    a=arr[i];
+    cg_inst.sample(); 
     $display("a=%d coverage %%=%0.2f",a,cg_inst.get_inst_coverage());
   end
 end
